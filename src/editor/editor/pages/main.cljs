3e0c22d108aa0f95d9560330b2473241e2ee9648
Andi Rusu <rusu.andy1@gmail.com>
2021-09-29 15:02:37 +0300

    Router issues

--- src/editor/editor/pages/main.cljs
+++ src/editor/editor/pages/main.cljs
@@ -1,27 +1,31 @@
 (ns editor.pages.main
   (:require
    ["react-router-dom" :refer (Switch Route Redirect) :rename {BrowserRouter router}]
-   [reagent.core :as r]
-   [common.auth :refer (is-signedin?)]
+   [common.hooks :as hooks]
    [editor.pages.login :as login]
    [editor.pages.stories :as stories]))
 
 (defn protected-route
   [options children]
-  [:> Route (assoc options :render (fn [params]
-                                     (if (is-signedin?)
-                                       children
-                                       [:> Redirect {:pathname "/login" :state {:from (.-location params)}}])))])
+  (let [auth (hooks/useAuth)
+        make-route (partial conj [:> Route options])]
+    (make-route
+     (if (:is-signedin? auth)
+       children
+       [:> Redirect {:to {:pathname (:redirect-to options) :state {:from (:path options)}}}]))))
 
 (defn show
   []
-  [:> router
-   [:> Switch
-    [protected-route {:exact true :path "/"}
-     [:h1 "Hello world"]]
-    [:> Route {:path "/login"}
-     [:f> login/show]]
-    [protected-route {:path "/stories"}
-     [:f> stories/show]]
-    [:> Route
-     [:h1 "oops..."]]]])
+  [:f> hooks/provide-auth
+   [:> router {:forceRefresh true}
+    [:> Switch
+     [:> Route {:path "/login"}
+      [:f> login/show]]
+     [:f> protected-route {:exact true :path "/" :redirect-to "/login"}
+      [:h1 "Hello world"]]
+     [:f> protected-route {:path "/stories" :redirect-to "/login"}
+      [:f> stories/show]]
+     [:> Route {:path "/logout"}
+      [:f> login/show]]
+     [:> Route {:path "*"}
+      [:h1 "oops..."]]]]])
